cf0b9aa0711e2d4f907c26f17d59e09d432671e6
hasan-upinion <44969442+hasan-upinion@users.noreply.github.com>
2019-03-26 19:16:12 +0100

    Update getQuestions helper
    Get the questions recursively.

--- helpers/index.js
+++ helpers/index.js
@@ -21,79 +21,36 @@ export function getIndex(arr, from, identifier) {
   return arr.findIndex(value => value[identifier] === from)
 }
 
-export function getQuestions(questionLimit) {
-  let qs = []
-  let used = {}
-  let limit = questionLimit || QUESTIONS_LIMIT
-  const keys = Object.keys(terms)
-  const len = keys.length
+const getRan = n => Math.floor(Math.random() * n);
 
-  let counter = 0
-  while (counter < limit) {
-    let randomNumber = Math.floor(Math.random() * len)
-    const ranArr = terms[keys[randomNumber]]
-    if (Array.isArray(ranArr)) {
-      let randomTermNum = Math.floor(Math.random() * ranArr.length)
-      randomTermNum = randomTermNum <= 1 ? randomTermNum + 2 : randomTermNum
-      if (!used[randomTermNum]) {
-        qs.push({ text: ranArr[randomTermNum], checked: false })
-        counter++
-        continue
-      }
-      used[randomTermNum] = true
-    } else {
-      const subTerms = Object.keys(ranArr)
-      const randomNumber1 = Math.floor(Math.random() * subTerms.length)
-      if (!used[randomNumber + '_' + randomNumber1]) {
-        const subTermArr = ranArr[subTerms[randomNumber1]]
-        let randomSubTermNum = Math.floor(Math.random() * subTermArr.length)
-        randomSubTermNum =
-          randomSubTermNum <= 1 ? randomSubTermNum + 2 : randomSubTermNum
-        qs.push({ text: subTermArr[randomSubTermNum], checked: false })
-        counter++
-        continue
-      }
-      used[randomNumber + '_' + randomNumber1] = true
+export function getQuestions(questionLimit = QUESTIONS_LIMIT) {
+    const output = [];
+    const hash = {}; //used ones; to not repeat them!
+    const keys = Object.keys(terms);
+
+    function getChoice(arr) {
+        if(output.length === limit) return;
+
+        const len = arr ? arr.length : keys.length;
+        const currArr = arr ? arr : terms[keys[getRan(len)]];
+        if (Array.isArray(currArr)) {
+            const ranN = getRan(currArr.length);
+            if (!hash[currArr[ranN]]) {
+                output.push(currArr[ranN]);
+				        hash[currArr[ranN]] = true;
+            }
+        } else {
+            // { key: [..], key1: [..], ... }
+            const keys1 = Object.keys(currArr);
+            const nextArr = currArr[keys1[getRan(keys1.length)]];
+            getChoice(nextArr);
+        }
+
+        if(output.length !== limit) {
+            getChoice();
+        }
+
+        return output;
     }
-  }
-  return qs
+  return getChoice();
 }
-
-// const used = {}
-// let qs = []
-// const limit = 4
-// const get = () => {
-//   const keys = Object.keys(terms)
-
-//   if (qs.length === limit) return qs
-
-//   let randomNumber = Math.floor(Math.random() * keys.length)
-//   const ranArr = terms[keys[randomNumber]]
-//   if (Array.isArray(ranArr)) {
-//     let randomTermNum = Math.floor(Math.random() * ranArr.length)
-//     randomTermNum = randomTermNum <= 1 ? randomTermNum + 2 : randomTermNum
-//     if (!used[randomTermNum]) {
-//       qs.push(ranArr[randomTermNum])
-//       used[randomTermNum] = true
-//     }
-//   } else {
-//     // category: { arg1: [...], arg2: [...] }
-//     const subTerms = Object.keys(ranArr)
-//     const randomNumber = Math.floor(Math.random() * subTerms.length)
-//     get()
-//   }
-
-//   if (qs.length < limit) get()
-
-//   return qs
-// }
-
-// console.time('Test')
-// console.log(`Calling it now...`)
-// console.log(get())
-// console.timeEnd('Test')
-
-// console.time('Test 1')
-// console.log('calling with while loop...')
-// console.log(getQuestions([]))
-// console.timeEnd('Test 1')
