968180e54e5c413e54359d3466311343476d8527
Hasan_sh <hasansh343@gmail.com>
2019-08-18 12:00:18 +0200

    RTL support

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import {
   Platform,
   StatusBar,
   StyleSheet,
   View,
-  I18nManager,
 } from 'react-native'
 import { Provider as PaperProvider } from 'react-native-paper'
 
 import AppNavigator from './navigation/AppNavigator'
 import StoreContext from './state'
 
-function forceRTL() {
-  try {
-    I18nManager.forceRTL(true)
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-const App = () => {
-  useEffect(() => {
-    forceRTL()
-  }, [])
-
-  return (
-    <StoreContext>
-      {/* <TimerStoreContext> */}
-      <PaperProvider>
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      </PaperProvider>
-      {/* </TimerStoreContext> */}
-    </StoreContext>
-  )
-}
+const App = () => (
+  <StoreContext>
+    <PaperProvider>
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    </PaperProvider>
+  </StoreContext>
+);
 
 export default App
 
