498bb73c00aba84656ad3aea7625c25fe17d64bd
hasan-sh <hasansh343@gmail.com>
2022-06-28 12:57:33 +0300

    Used themes for navigation and rn-paper

--- App.js
+++ App.js
@@ -7,21 +7,32 @@ import {
 } from 'react-native'
 import { Provider as PaperProvider, Title } from 'react-native-paper'
 
+import { useColorScheme } from 'react-native';
+
+
+import FocusAwareStatusBar from './components/FocusedStatusBar'
+
 import AppNavigator from './navigation/AppNavigator'
 import StoreContext from './state'
 import FirebaseState from './state/firebase'
+import { CombinedDarkTheme, CombinedDefaultTheme } from './constants/Theme';
 
-const App = () => (
-  <StoreContext>
-    {/* <FirebaseState /> */}
-    <PaperProvider>
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    </PaperProvider>
-  </StoreContext>
-);
+const App = () => {
+  const scheme = useColorScheme();
+  const theme = scheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  return (
+    <StoreContext >
+      {/* <FirebaseState /> */}
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <FocusAwareStatusBar barStyle="default" />}
+          <AppNavigator theme={theme} />
+        </View>
+      </PaperProvider>
+    </StoreContext>
+  )
+}
 
 export default App
 
